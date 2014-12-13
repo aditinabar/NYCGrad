3cc56b3b2f821f41bf98c6d9d7d7926e7fd24ac5
aditinabar <ahnabar@gmail.com>
2014-12-12 22:20:48 -0600

    initial commit

---
+++ CondenseDataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pandas import DataFrame
+
+df = pd.read_csv('Graduation_Outcomes_by_SchoolLevel_Gender_2005-2011.csv')
+df2 = df[df['Total Cohort Num'] > 5]
+df3 = df2[range(1,8)]
+
+print df3
+
+df3.to_csv('Grad_by_SchoolGender.csv')
