4fa384ab518c0d90a9fb2d9d28dfd934bbbdd25c
aditinabar <ahnabar@gmail.com>
2014-12-14 00:12:36 -0600

    update: index_col = School Name

--- CondenseDataset.py
+++ CondenseDataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pandas import DataFrame
 
-df = pd.read_csv('Graduation_Outcomes_by_SchoolLevel_Gender_2005-2011.csv')
+df = pd.read_csv('Graduation_Outcomes_by_SchoolLevel_Gender_2005-2011.csv', index_col = 'School Name')
 df2 = df[df['Total Cohort Num'] > 5]
 df3 = df2[range(1,8)]
 
