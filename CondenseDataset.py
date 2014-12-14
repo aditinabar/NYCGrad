66f8af3c07f37da7d3dc6e9e5d92ad2ce881ff37
aditinabar <ahnabar@gmail.com>
2014-12-14 17:03:48 -0600

    Comments added

--- CondenseDataset.py
+++ CondenseDataset.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from pandas import DataFrame
-
+#Read CSV, make 'School Name' first column
 df = pd.read_csv('Graduation_Outcomes_by_SchoolLevel_Gender_2005-2011.csv', index_col = 'School Name')
+
+#Select only schools with a cohort of at least 5
 df2 = df[df['Total Cohort Num'] > 5]
+
+#Subset df2 to include only the first 7 vectors
 df3 = df2[range(1,8)]
 
-print df3
+print df3.head()
 
-df3.to_csv('Grad_by_SchoolGender.csv')
+#df3.to_csv('Grad_by_SchoolGender.csv')
