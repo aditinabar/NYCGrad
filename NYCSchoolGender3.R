ac95070f04a064e7302262ebde93d8b1c477304d
aditinabar <ahnabar@gmail.com>
2015-04-11 15:13:04 -0500

    Subset by Year; overall ttest

--- NYCSchoolGender3.R
+++ NYCSchoolGender3.R
@@ -3,7 +3,9 @@ getwd()
 library(ggplot2)
 
 
-NYC = read.csv("Grad_by_SchoolGender.csv", header = TRUE)
+NYC = read.table("Grad_by_SchoolGender.csv", header = TRUE, stringAsFactor = TRUE)
+
+NycC = read.csv("NYC_Clean.csv", header = TRUE)
 #copy
 data <- NYC
 
@@ -23,10 +25,23 @@ repS <- as.numeric(repS)
 
 data$Total.Grads.Num <- repS
 
+#write 'data' to file 
+write.csv(data, file = "NYC_Clean.csv", row.names = FALSE)
+
 #subset data: 2006, Female Male
+data.2001 <- subset(data, c(data$Cohort.Year == 2001, 
+                            data$Cohort.Category == "* June"), 
+                    row.names = FALSE)
+data.2002 <- subset(data, data$Cohort.Year == 2002)
+data.2003 <- subset(data, data$Cohort.Year == 2003)
+data.2004 <- subset(data, data$Cohort.Year == 2004)
+data.2005 <- subset(data, data$Cohort.Year == 2005)
 data.2006 <- subset(data, data$Cohort.Year == 2006)
+data.2007 <- subset(data, data$Cohort.Year == 2007)
+
 table(data$Cohort.Category, data$Cohort.Year)
-tapply(data.2006$Total.Grads.Pct.of.cohort, data.2006$Cohort.Category, mean, na.rm = TRUE)
+tapply(data.2006$Total.Grads.Pct.of.cohort, 
+       data.2006$Cohort.Category, mean, na.rm = TRUE)
 Fem2006 <- subset(data.2006, data.2006$Demographic == "Female")
 Male2006 <- subset(data.2006, data.2006$Demographic == "Male")
 
@@ -35,3 +50,21 @@ tapply(Male2006$Total.Grads.Pct.of.cohort, Male2006$Cohort.Category, mean, na.rm
 
 tapply(Fem2006$Total.Grads.Num, Fem2006$Cohort.Category, mean, na.rm = TRUE)
 tapply(Male2006$Total.Grads.Num, Male2006$Cohort.Category, mean, na.rm = TRUE)
+
+#plot histogram
+J2006.lt50 <- subset(Jdata.2006, Jdata.2006$Total.Grads.Pct.of.cohort < 50)
+
+g2 <- ggplot(J2006.lt50, aes(Total.Grads.Pct.of.cohort))
+g2 + geom_histogram()
+
+#boxplot
+boxplot(data$Total.Grads.Pct.of.cohort[data$Demographic == "Female"], 
+        data$Total.Grads.Pct.of.cohort[data$Demographic == "Male"], 
+        ylab = "Grads as Percent of Cohort", names = c("Female", "Male"), 
+        main = "Overall F v M")
+
+#Overall Comparison
+t.test(data$Total.Grads.Pct.of.cohort[data$Demographic == "Female"], 
+       data$Total.Grads.Pct.of.cohort[data$Demographic == "Male"])
+
+
