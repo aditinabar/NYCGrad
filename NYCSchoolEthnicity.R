f134eed0950ab0624feb73387febb8a251a52561
aditinabar <ahnabar@gmail.com>
2015-05-22 13:53:43 -0500

    remove function, keep for loop

--- NYCSchoolEthnicity.R
+++ NYCSchoolEthnicity.R
@@ -5,12 +5,8 @@ eth <- read.table("Graduation_Outcomes_by_SchoolLevel_Ethnicity_2005-2011.csv", 
 row.names(eth) <- NULL
 eth[,1] <- NULL
 
-rmregents <- function(data){
-  for(i in 8:16){
-    data[,i] <- NULL
-  }
+for(i in 8:16){
+  data[,i] <- NULL
 }
-
-rmregents(data = eth)
 
 View(eth)
